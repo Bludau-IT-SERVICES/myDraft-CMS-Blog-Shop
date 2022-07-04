b5a477419c8fcfddea32fecbe671be86d34071b6
Jan Bludau <the_invincible@live.de>
2022-07-04 13:44:07 +0200

    6t commit

---
+++ install/myDraft Datenbank Schema.sql
@@ -0,0 +1,191 @@
+-- --------------------------------------------------------
+-- Host:                         85.215.101.24
+-- Server Version:               10.6.8-MariaDB-1:10.6.8+maria~buster-log - mariadb.org binary distribution
+-- Server Betriebssystem:        debian-linux-gnu
+-- HeidiSQL Version:             12.0.0.6531
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
